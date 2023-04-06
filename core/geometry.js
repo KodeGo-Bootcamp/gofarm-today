eb64f7a073183475e2eb9af901e63debd2ec9f09
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-04-06 21:22:08 +0800

    Add degree to radian converter

---
+++ core/geometry.js
@@ -0,0 +1,10 @@
+/**
+ * Convert an angle in degree to radian
+ * @param {Number} theta Angle in degree
+ * @returns Angle in radian
+ */
+function toRadian(theta) {
+    const radian = theta * Math.PI / 180
+    return radian
+}
+
