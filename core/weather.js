33eb4a05ae84ee6e513688ca031ec45fa8dbdfcb
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-04-10 19:39:16 +0800

    Add Unix to JavaScript timestamp converter

---
+++ core/weather.js
@@ -0,0 +1,10 @@
+/**
+ * Convert Unix timestamp to JavaScript
+ * @param {Number} timestamp Unix timestamp
+ * @returns JavaScript timestamp
+ */
+function toJsTimestamp(timestamp) {
+    const jsTimestamp = timestamp * 1000
+    return jsTimestamp
+}
+
