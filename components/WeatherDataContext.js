caf2fa975959718510701864f7af3721197da37f
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-05-01 22:25:25 +0800

    Add minimal working feature

---
+++ components/WeatherDataContext.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useReducer } from "react";
+
+const WeatherDataContext = createContext()
+const WeatherDataDispatchContext = createContext()
+
+function weatherDataReducer(state, action) {
+    switch (action.type) {
+        case "UPDATE":
+            return {
+                ...action.data
+            }
+        default:
+            throw Error("Unknown action: " + action.type)
+    }
+}
+
+export function useWeatherDataReducer() {
+    return [useContext(WeatherDataContext), useContext(WeatherDataDispatchContext)]
+}
+
+export function WeatherDataProvider({ children }) {
+    const [weatherData, weatherDataDispatch] = useReducer(weatherDataReducer)
+
+    return (
+        <WeatherDataContext.Provider value={weatherData}>
+            <WeatherDataDispatchContext.Provider value={weatherDataDispatch}>
+                {children}
+            </WeatherDataDispatchContext.Provider>
+        </WeatherDataContext.Provider>
+    )
+}
