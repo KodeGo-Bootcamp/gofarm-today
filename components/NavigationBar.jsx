9ae593940de810ce18c4aec9f4c193f59353955a
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-05-03 23:14:27 +0800

    Update navigation bar background

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -12,10 +12,10 @@ export default function NavigationBar({ activeKey }) {
 
     const LogoShownOnMobile = () => {
         return (
-            <Navbar className={"d-md-none p-3 pb-0"} bg={"dark"} variant={"dark"}>
+            <Navbar className={"d-md-none p-3 pb-0"} bg={"light"}>
                 <Container className="justify-content-center">
                     <Navbar.Brand className={"m-0 p-0 pt-3"} href={getLink("/")} aria-label={"home"}>
-                        <Image src={"/cover-gofarm.svg"} width={104 * 1.3 * 2} height={30.9971127298 * 1.3 * 2} alt={"go farm cover"} />
+                        <Image src={"/cover-gofarm.svg"} width={104 * 1.3 * 2} height={30.9971127298 * 1.3 * 2} alt={"go farm cover"} loading={"eager"} />
                     </Navbar.Brand>
                 </Container>
             </Navbar>
@@ -24,7 +24,7 @@ export default function NavigationBar({ activeKey }) {
 
     const NavigationLinks = () => {
         return (
-            <Navbar className={"p-3"} sticky={"top"} bg={"dark"} variant={"dark"}>
+            <Navbar className={"p-3"} sticky={"top"} bg={"light"}>
                 <Container className={"justify-content-center justify-content-md-start"}>
                     <Navbar.Brand className={"d-none d-md-inline p-0"} href={getLink("/")} aria-label={"home"}>
                         <Image src={"/logo-gofarm.svg"} width={30.9971127298 * 1.3} height={30.9971127298 * 1.3} alt={"go farm logo"} />
